382d2276a9ffff760c8c622bfa314d6b56b086a8
laravoigt <21572631@sun.ac.za>
2022-09-13 20:00:50 +0200

    test page updated

--- Next/github-social-media/pages/test.js
+++ Next/github-social-media/pages/test.js
@@ -4,29 +4,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import React from 'react';
 
 function test() {
   return (
     <div>
-      <Stack direction="horizontal" variant="dark">
-        <Button as="a" variant="success">
-          Button as link
-        </Button>
-        <Button as="a" variant="success">
-          Button as link
-        </Button>
-      </Stack>
-      <Container>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col xs={6}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col xs={5}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
+      <div className="p-2 bg-light border">
+        <Container class="container-fluid" >
+          <ul class="list-group">
+            <Row >
+              <Col><li class="list-group-item">username</li></Col>
+            </Row>
+            <Row>
+              <Col><li class="list-group-item">starred topics</li></Col>
+            </Row>
+            <Row>
+              <Col><li class="list-group-item">starred repositories</li></Col>
+            </Row>
+          </ul>
+        </Container>
+      </div>
+      <Container class="container-fluid">
+        <Row>
+        <Col sm={8}>
+        <ul class="list-group">
+           <li class="list-group-item">Based on topics you've starred</li>
+           <li class="list-group-item">koopsmans-functionals</li>
+           <li class="list-group-item">ase_koopsmans</li>
+        </ul>
+        </Col>
+        <Col>
+        <ul class="list-group">
+        <li class="list-group-item">Trending repositories</li>
+        <li class="list-group-item">Python</li>
+        <li class="list-group-item">surrealdb</li>
+        </ul>
+        </Col>
+        </Row>
     </Container>
     </div>
   )
