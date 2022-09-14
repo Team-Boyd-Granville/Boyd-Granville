8a8d8935e36ba22c6dc66e3bc4cf5d99c1ce5d9d
GARY JARED Steyn <garyjaredsteyn@gmail.cm>
2022-09-14 13:00:41 +0200

    db config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,1 @@
-INSERT INTO Users (UserID, FirstName, LastName, email)
-VALUES ('1', 'Tom B. Erichsen', 'Skagen', 'email@test.com');
+INSERT INTO Users (UserID, FirstName, LastName, email) VALUES (1, 'Tom B. Erichsen', 'Skagen', 'email@test.com');
