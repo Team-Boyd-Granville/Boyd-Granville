8f650612f172202200a12050fff08c7043951da9
GARY JARED Steyn <garyjaredsteyn@gmail.cm>
2022-10-07 16:23:30 +0200

    test

--- src/main/java/boyd/api/service/UserService.java
+++ src/main/java/boyd/api/service/UserService.java
@@ -98,8 +98,8 @@ public class UserService {
     public String getRecommendations(String username) {
         String node = username;
         // for(int i = 0; i < 3; i++) {
-            String[] favourite = getStarred(node).split(" ");
-            node = favourite[2].substring(0, favourite[2].length()-2);
+            String[] favourite = getStarred(node).split("\n");
+            // node = favourite[0].substring(0, favourite[2].length()-2);
             // favourite = getStarred(node).split(" ");
             // node = favourite[2].substring(0, favourite[2].length()-2);
         // }
