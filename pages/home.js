cc7da80c6d00e51706991399c6abfb280a4b3923
DANIEL JASON Haupt <23640588@sun.ac.za>
2022-10-13 16:52:07 +0200

    cleaned up info displayed a bit

--- pages/home.js
+++ pages/home.js
@@ -16,138 +16,138 @@ import { getUsersRepos, getUsersStarred, getRecommendedRepos, getUserInfo } from
 
 
 function Home() {
-    const { data: session, status } = useSession();
-    const [starred, setStarred] = useState([]);
-    const [recommended, setRecommended] = useState([]);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [repos, setRepos] = useState([]);
-    const [userInfo, setUserInfo] = useState({})
+  const { data: session, status } = useSession();
+  const [starred, setStarred] = useState([]);
+  const [recommended, setRecommended] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [repos, setRepos] = useState([]);
+  const [userInfo, setUserInfo] = useState({})
 
-    
-    
-    useEffect(() => {
-      const getUserDetails = () => {
-            getUserInfo(session.user.name)
-                .then(resp => {
-                  console.log(resp);
-                  setUserInfo(resp);
-              })
-              .catch(error => {
-                  alert("Error getting repos, see log");
-                  console.log(error);
-              });
-      };
-      const getStarred = () => {
-          getUsersStarred(session.user.name)
-              .then(resp => {
-                  console.log(resp);
-                  setStarred(resp);
-              })
-              .catch(error => {
-                  alert("Error getting starred repos, see log");
-                  console.log(error);
-              });
-      };
-      const getRecommended = () => {
-        getRecommendedRepos(session.user.name, pageNumber)
-            .then(resp => {
-                console.log(resp);
-                setRecommended(r => {
-                return [...r, ...resp]
-                });
-            })
-            .catch(error => {
-                alert("Error getting recommended repos, see log");
-                console.log(error);
-            });
+
+
+  useEffect(() => {
+    const getUserDetails = () => {
+      getUserInfo(session.user.name)
+        .then(resp => {
+          console.log(resp);
+          setUserInfo(resp);
+        })
+        .catch(error => {
+          alert("Error getting repos, see log");
+          console.log(error);
+        });
     };
-      if (session !== undefined) {
-            getUserDetails();
-            getStarred();
-            getRecommended();
-      }
-    }, [session, pageNumber]);
+    const getStarred = () => {
+      getUsersStarred(session.user.name)
+        .then(resp => {
+          console.log(resp);
+          setStarred(resp);
+        })
+        .catch(error => {
+          alert("Error getting starred repos, see log");
+          console.log(error);
+        });
+    };
+    const getRecommended = () => {
+      getRecommendedRepos(session.user.name, pageNumber)
+        .then(resp => {
+          console.log(resp);
+          setRecommended(r => {
+            return [...r, ...resp]
+          });
+        })
+        .catch(error => {
+          alert("Error getting recommended repos, see log");
+          console.log(error);
+        });
+    };
+    if (session !== undefined) {
+      getUserDetails();
+      getStarred();
+      getRecommended();
+    }
+  }, [session, pageNumber]);
 
-  if (session !== undefined) 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-3">
-        <div className="container mb-2">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={session.user.image} />
-          <Card.Body>
-            <Card.Title>{session.user.name}</Card.Title>
-            <Card.Text>
-              Bio: {JSON.stringify(userInfo.bio)}
-            </Card.Text>
-            <Card.Text>
-              Blog: {JSON.stringify(userInfo.blog)}
-            </Card.Text>
-            <Card.Text>
-              Followers: <Link href={'/followers'}><a style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.followers)}</a></Link>
-            </Card.Text>
-            <Card.Text>
-              Following: <Link href={'/following'}><a style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.following)}</a></Link>
-            </Card.Text>
-            </Card.Body>
-        </Card>
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Text>
-              Company: {JSON.stringify(userInfo.company)}
-            </Card.Text>
-            <Card.Text>
-              Location: {JSON.stringify(userInfo.location)}
-            </Card.Text>
-            <Card.Text>
-              Public Repos: <a href={'/home'} style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.public_repos)}</a>
-            </Card.Text>
-            <Card.Text>
-              Public Gists: {JSON.stringify(userInfo.public_gists)}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        </div>
-        </div>
-        <div className="col-5">
-        
-        <div className="container mb-5">
-            <div className="list-group">
-              <a className="list-group-item list-group-item-action active list-group-item-secondary" aria-current="true">
-                <div className="d-flex w-100 justify-content-between">
-                  <h5 className="mb-1">What we found based on your interests:</h5>
-                </div>
-                {/* <small>There are 3 public repositories you might be interested in</small> */}
-              </a>
+  if (session !== undefined)
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <div className="container mb-2">
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={session.user.image} />
+                <Card.Body>
+                  <Card.Title>{session.user.name}</Card.Title>
+                  <Card.Text>
+                    Bio: {JSON.stringify(userInfo.bio)}
+                  </Card.Text>
+                  <Card.Text>
+                    Blog: {JSON.stringify(userInfo.blog)}
+                  </Card.Text>
+                  <Card.Text>
+                    Followers: <Link href={'/followers'}><a style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.followers)}</a></Link>
+                  </Card.Text>
+                  <Card.Text>
+                    Following: <Link href={'/following'}><a style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.following)}</a></Link>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Text>
+                    Company: {JSON.stringify(userInfo.company)}
+                  </Card.Text>
+                  <Card.Text>
+                    Location: {JSON.stringify(userInfo.location)}
+                  </Card.Text>
+                  <Card.Text>
+                    Public Repos: <a href={'/home'} style={{ textDecoration: 'none' }}>{JSON.stringify(userInfo.public_repos)}</a>
+                  </Card.Text>
+                  <Card.Text>
+                    Public Gists: {JSON.stringify(userInfo.public_gists)}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <div className="col-5">
 
-    <div style={{overflow: "scroll", height : "400px"}}>
-    {/* <h1>{JSON.stringify(recommended)}</h1> */}
-      {(typeof recommended === 'undefined') ? (
-        <h1></h1>
-        ) : (
-          recommended.map((repodata) => (
-            // <Link href={'/starredRepositories/'+starredRepo.substring(0, starredRepo.indexOf(','))}>
-              <a href="#" className="list-group-item list-group-item-action">
-                <div className="d-flex w-100 justify-content-between">
-                  <h5 className="mb-1">{repodata}</h5>
-                  
-                  <small className="text-muted">3 days ago</small>
-                </div>
-                <p className="mb-1">This topic is about ...</p>
-                <small className="text-muted">because you follow xyz</small>
-              </a>
-          // </Link>
-          ))
-          )}
-          <a href="#" className="list-group-item list-group-item-action">
-                <div className="d-flex w-100 justify-content-between">
-          <a style={{position : "relative", display : "block", left : "40%", width : "200px"}} onClick={()=>{setPageNumber(pageNumber + 1)}}>View more</a>
-                </div>
-              </a>
-    </div>
+            <div className="container mb-5">
+              <div className="list-group">
+                <a className="list-group-item list-group-item-action active list-group-item-secondary" aria-current="true">
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">What we found based on your interests:</h5>
+                  </div>
+                  {/* <small>There are 3 public repositories you might be interested in</small> */}
+                </a>
 
-              {/* <a href="#" className="list-group-item list-group-item-action">
+                <div style={{ overflow: "scroll", height: "400px" }}>
+                  {/* <h1>{JSON.stringify(recommended)}</h1> */}
+                  {(typeof recommended === 'undefined') ? (
+                    <h1></h1>
+                  ) : (
+                    recommended.map((repodata) => (
+                      <Link href={'/starredRepositories/' + repodata.split(",")[0].trim() + '*' + repodata.split(",")[2].replace(".", "").trim()}>
+                        <a href="#" className="list-group-item list-group-item-action">
+                          <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">{repodata.split(",")[0].trim()}</h5>
+
+                            {/* <small className="text-muted">3 days ago</small> */}
+                          </div>
+                          <p className="mb-1">This topic is about ...</p>
+                          <small className="text-muted">because you follow xyz</small>
+                        </a>
+                      </Link>
+                    ))
+                  )}
+                  <a href="#" className="list-group-item list-group-item-action">
+                    <div className="d-flex w-100 justify-content-between">
+                      <a style={{ position: "relative", display: "block", left: "40%", width: "200px" }} onClick={() => { setPageNumber(pageNumber + 1) }}>View more</a>
+                    </div>
+                  </a>
+                </div>
+
+                {/* <a href="#" className="list-group-item list-group-item-action">
                 <div className="d-flex w-100 justify-content-between">
                   <h5 className="mb-1">Repository 2</h5>
                   <small className="text-muted">4 days ago</small>
@@ -163,10 +163,10 @@ function Home() {
                 <p className="mb-1">This topic is about ... </p>
                 <small className="text-muted">because you starred xyz</small>
               </a> */}
+              </div>
             </div>
-          </div>
-    
-          {/* <div className="container mb-5">
+
+            {/* <div className="container mb-5">
             <div className="list-group">
               <a className="list-group-item list-group-item-action active list-group-item-light" aria-current="true">
                 <div className="d-flex w-100 justify-content-between">
@@ -200,32 +200,32 @@ function Home() {
             </div>
           </div> */}
 
-          <div className="container mb-5">
-            <div className="list-group">
-              <a className="list-group-item list-group-item-action active list-group-item-warning" aria-current="true">
-                <div className="d-flex w-100 justify-content-between">
-                  <h5 className="mb-1">Starred Projects:</h5>
-                </div>
-              </a>
+            <div className="container mb-5">
+              <div className="list-group">
+                <a className="list-group-item list-group-item-action active list-group-item-warning" aria-current="true">
+                  <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">Starred Projects:</h5>
+                  </div>
+                </a>
 
-      {(typeof starred === 'undefined') ? (
-          <h1></h1>
-      ) : (
-        starred.map((starredRepo) => (
-          <Link href={'/starredRepositories/'+starredRepo.split(",")[0].trim()+'*'+starredRepo.split(",")[2].replace(".","").trim()}>
-            {/* starredRepo.split(",")[1].trim() */}
-          <a className="list-group-item list-group-item-action">
-          <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{starredRepo}</h5>
-              <small className="text-muted">5 days ago</small>
-            </div>
-            <p className="mb-1">latest update </p>
-            <small className="text-muted">-user-</small>
-          </a>
-          </Link>
-        ))
-        )}
-              {/* <a href="#" className="list-group-item list-group-item-action">
+                {(typeof starred === 'undefined') ? (
+                  <h1></h1>
+                ) : (
+                  starred.map((starredRepo) => (
+                    <Link href={'/starredRepositories/' + starredRepo.split(",")[0].trim() + '*' + starredRepo.split(",")[2].replace(".", "").trim()}>
+                      {/* starredRepo.split(",")[1].trim() */}
+                      <a className="list-group-item list-group-item-action">
+                        <div className="d-flex w-100 justify-content-between">
+                          <h5 className="mb-1">{starredRepo.split(",")[0].trim()}</h5>
+                          {/* <small className="text-muted">5 days ago</small> */}
+                        </div>
+                        {/* <p className="mb-1">latest update </p>
+                        <small className="text-muted">-user-</small> */}
+                      </a>
+                    </Link>
+                  ))
+                )}
+                {/* <a href="#" className="list-group-item list-group-item-action">
               <div className="d-flex w-100 justify-content-between">
                   <h5 className="mb-1">Repository 1</h5>
                   <small className="text-muted">5 days ago</small>
@@ -249,9 +249,9 @@ function Home() {
                 <p className="mb-1">latest update </p>
                 <small className="text-muted">-user-</small>
               </a> */}
+              </div>
             </div>
-          </div>
-{/*     
+            {/*     
           <div className="container mb-5">
             <div className="list-group">
               <a className="list-group-item list-group-item-action active list-group-item-warning" aria-current="true">
@@ -285,10 +285,10 @@ function Home() {
               </a>
             </div>
           </div> */}
-    
-        </div>
-    
-        {/* <div className="col-4">
+
+          </div>
+
+          {/* <div className="col-4">
         <div className="container mb-5">
             <div className="list-group">
               <a className="list-group-item list-group-item-action active list-group-item-primary" aria-current="true">
@@ -322,18 +322,18 @@ function Home() {
   
     
         </div> */}
-        
+
+        </div>
       </div>
-    </div>
-    
-        
-    
-      )
+
+
+
+    )
 }
 
 Home.getLayout = function getLayout(Home) {
   return (
-      <Layout>{Home}</Layout>
+    <Layout>{Home}</Layout>
   )
 }
 
